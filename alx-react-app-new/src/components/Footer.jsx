bb5c78c608e4e9b7a839df86176224461b99ea5b
Chiamaka Uyanna <Chiamakauyanna@gmail.com>
2024-08-12 11:39:41 +0100

    Duplicate alx-react-app and add styling

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p style={{ fontWeight: 'bold', fontSize: 14 , textAlign: "right", width: "100%", marginTop: "1em", paddingRight: "1em"}}>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main>
+       <p style={{ fontWeight: 'bold', fontSize: 24 , textAlign: "center", width: "100%", height: "200px", marginTop: "7em"}}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
