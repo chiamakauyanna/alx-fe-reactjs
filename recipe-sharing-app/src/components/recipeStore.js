6157fc7bd496272316254cdf00aa87d7e7a22821
Chiamaka Uyanna <Chiamakauyanna@gmail.com>
2024-08-24 12:25:43 +0100

    Add Update and delete methods

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -14,6 +14,18 @@ const useRecipeStore = create((set) => ({
       recipes,
     })),
 
+  updateRecipe: (recipeId, updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === recipeId ? { ...recipe, ...updatedRecipe } : recipe
+      ),
+    })),
+
+  deleteRecipe: (recipeId) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
+    })),
+
   addFavorite: (recipeId) =>
     set((state) => ({ favorites: [...state.favorites, recipeId] })),
 
