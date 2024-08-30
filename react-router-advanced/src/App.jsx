4c43f17908151c51678090b0459de2a5e989b0dd
Chiamaka Uyanna <Chiamakauyanna@gmail.com>
2024-08-30 16:26:20 +0100

    minor: changed routing pattern

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import UserLogin from './auth/UserLogin';
 import Profile from './components/Profile';
@@ -13,45 +9,27 @@ import UserProfile from './components/UserProfile';
 import ProtectedRoute from './components/protectedRoute';
 
 
-const isLoggedIn = true; // Simulate authentication status
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/userlogin',
-    element: <UserLogin />,
-  },
-  {
-    path: '/profile',
-    element: (
-      <ProtectedRoute isLoggedIn={isLoggedIn}>
-        <Profile />
-      </ProtectedRoute>
-    ),
-    children: [
-      {
-        path: 'profile-details',
-        element: <ProfileDetails />,
-      },
-      {
-        path: 'profile-settings',
-        element: <ProfileSettings />,
-      },
-    ],
-  },
-  {
-    path: '/user/:userId',
-    element: <UserProfile />,
-  },
-]);
-
 const App = () => {
+  const isLoggedIn = false; // Simulate authentication status
+
   return (
-    <RouterProvider router = { router } />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/userlogin" element={<UserLogin />} />
+        <Route path="/profile" element={
+          <ProtectedRoute isLoggedIn={isLoggedIn}>
+            <Profile />
+          </ProtectedRoute>
+        }>
+          <Route path="profile-details" element={<ProfileDetails />} />
+          <Route path="profile-settings" element={<ProfileSettings />} />
+        </Route>
+        <Route path="/user/:userId" element={<UserProfile />} />
+      </Routes>
+    </Router>
   );
+
 };
  
 export default App;
