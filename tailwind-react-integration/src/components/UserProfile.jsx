2dc69e89e28ec43e9a3e18e7994edd2ddeafd613
Chiamaka Uyanna <Chiamakauyanna@gmail.com>
2024-09-06 13:48:57 +0100

    Styles: Added responsiveness

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,8 +1,8 @@
 const UserProfile = () => {
   return (
-    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-36 h-36 mx-auto"/>
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
+    <div className="bg-gray-100 p-4 md:p-8 max-w-sm md:max-w-xs mx-auto my-20 rounded-lg shadow-lg">
+      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full md:w-36 w-24 md:h-36 h-24 mx-auto"/>
+      <h1 className="text-sm md:text-xl text-blue-800 my-4">John Doe</h1>
       <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
