057cb7c441f6ac9047db6c063162d219536fd9d3
Chiamaka Uyanna <Chiamakauyanna@gmail.com>
2024-09-09 22:40:04 +0100

    Add grid styling

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
     }, []);
 
     return (
-        <div className="grid grid-cols-1 gap-8 justify-items-center md:grid-cols-2 md:gap-2 lg:grid-cols-3 my-8">
+        <div className="grid grid-cols-1 gap-8 justify-items-center md:grid-cols-2 lg:grid-cols-3 my-8">
             {data.map((recipe) => (
                 <div key={recipe.id} className="">
                    <div className="bg-green-100 w-64 md:w-60 md:h-90 flex flex-col rounded shadow hover:scale-110 transition-transform duration-500 ease-in-out">
