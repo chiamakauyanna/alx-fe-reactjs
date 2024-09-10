cd85da76bff0f960639b3216cd96a449d3d8916d
Chiamaka Uyanna <Chiamakauyanna@gmail.com>
2024-09-10 15:55:49 +0100

    Added a Form for adding new recipes

---
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from 'react'
+
+const AddRecipeForm = () => {
+  const [formData, setFormData] = useState({
+    title: '',
+    ingredients: '',
+    instructions: '',
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const validateForm = () => {
+    let formErrors = {};
+
+   
+    if (!formData.title) {
+      formErrors.title = 'Title is required';
+    }
+
+    const ingredientsArray = formData.ingredients.split(',').map((item) => item.trim());
+    if (!formData.ingredients) {
+      formErrors.ingredients = 'Ingredients are required';
+    } else if (ingredientsArray.length < 2) {
+      formErrors.ingredients = 'Please provide at least two ingredients';
+    }
+
+    if (!formData.instructions) {
+      formErrors.instructions = 'Instructions is required';
+    }
+
+    setErrors(formErrors);
+
+    return Object.keys(formErrors).length === 0;
+      };
+
+      const handleSubmit = (e) => {
+        e.preventDefault();
+    
+        if (validateForm()) {
+          console.log('Form submitted:', formData);
+        } else {
+          console.log('Form has errors');
+        }
+      };
+
+
+  return (
+    <div className='container m-auto h-screen flex flex-col items-center justify-center'>
+      <h1 className='mb-5 uppercase font-bold '>Add your favourite recipes</h1>
+      <form onSubmit={handleSubmit} className=' flex
+    flex-col'>
+      <div className=''>
+          <label className="block text-sm font-medium text-gray-700">Recipe Title</label>
+          <input
+            type="text"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            className="mt-1 p-2 block w-[15rem] md:w-[20rem] border border-gray-300 rounded-md"
+          />
+          {errors.title && <p className="text-red-500 text-sm">{errors.title}</p>}
+        </div>
+
+        {/* Ingredients */}
+        <div>
+          <label className="block text-sm font-medium text-gray-700">Preparations Steps</label>
+          <input
+            type="text"
+            name="ingredients"
+            value={formData.ingredients}
+            onChange={handleChange}
+            className="mt-1 p-2 block w-[15rem] md:w-[20rem] border border-gray-300 rounded-md"
+          />
+          {errors.ingredients && <p className="text-red-500 text-sm">{errors.ingredients}</p>}
+        </div>
+
+        {/* Instructions */}
+        <div>
+          <label className="block text-sm font-medium text-gray-700">Instructions</label>
+          <textarea
+            name="instructions"
+            value={formData.instructions}
+            onChange={handleChange}
+            className="mt-1 p-2 block w-[15rem] md:w-[20rem] border border-gray-300 rounded-md"
+          />
+          {errors.instructions && <p className="text-red-500 text-sm">{errors.instructions}</p>}
+        </div>
+
+        {/* Submit Button */}
+        <div>
+          <button
+            type="submit"
+            className="w-[15rem] md:w-[20rem] bg-green-800 text-white py-2 px-4 mt-4 rounded-md hover:bg-green-600 hover:transition-transform"
+          >
+            Submit Recipe
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default AddRecipeForm
