77f91aa04a3f87e696f6b77a42e15386b5a87782
Chiamaka Uyanna <Chiamakauyanna@gmail.com>
2024-09-10 13:51:09 +0100

    Added Links

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Recipe from '../data.json'
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
     const [data, setdata] = useState([]);
@@ -10,14 +10,14 @@ const HomePage = () => {
     }, []);
 
     return (
-        <div className="grid grid-cols-1 gap-8 justify-items-center md:grid-cols-2 lg:grid-cols-3 my-8">
+        <div className="bg-green-950 md:h-screen w-screen flex flex-col gap-8 items-center justify-center md:flex-row py-7 md:items-center">
             {data.map((recipe) => (
-                <div key={recipe.id} className="">
-                   <div className="bg-green-100 w-64 md:w-60 md:h-90 flex flex-col rounded shadow hover:scale-110 transition-transform duration-500 ease-in-out">
-                        <img src={recipe.image} alt="food" className="rounded h-52" />
+                 <div key={recipe.id} className="bg-green-100 w-64 md:w-60 flex flex-col rounded shadow hover:scale-110 transition-transform duration-500 ease-in-out">
+                    <Link to={`/recipe/${recipe.id}`}>
+                        <img src={recipe.image} alt="food" className="rounded h-52 w-full object-cover" />
                         <h1 className="text-green-800 text-base font-bold px-3 mt-6 uppercase hover:text-green-700">{recipe.title}</h1>
                         <p className="max-w-72 mt-2 mb-7 px-3 text-gray-600 text-sm">{recipe.summary}</p>
-                   </div>
+                    </Link>
                 </div>
             ))}
         </div>
