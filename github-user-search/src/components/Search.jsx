e956775fbaf7f5f1e8ae6aa9ce48b767a80207fe
Chiamaka Uyanna <Chiamakauyanna@gmail.com>
2024-09-19 16:26:17 +0100

    minor: adjust styling

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -26,7 +26,7 @@ const Search = () => {
 
   return (
     <div className="max-w-4xl mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4 text-blue-700 text-center pt-3">GitHub User Search</h1>
+      <h1 className="text-2xl font-bold mb-4 mt-20 text-blue-700 text-center pt-3">GitHub User Search</h1>
       <form onSubmit={handleSubmit} className="space-y-3 flex flex-col items-center">
         <div>
           <label className="block text-md text-blue-500 font-medium">Username:</label>
